0ce3725cb882be00f7570e75cab97b16613698ad
Hendre <hendre@mn8.ee>
2021-02-09 16:00:08 +0200

    test commit

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
-import (
-	"context"
-
-	"google.golang.org/grpc"
-	"intern.mn8.ee/arash/intern"
-)
-
 func main() {
 
-	conn, _ := grpc.Dial("localhost:12345", grpc.WithInsecure())
+	// conn, _ := grpc.Dial("localhost:12345", grpc.WithInsecure())
 
-	client := intern.NewGigsafeClient(conn)
+	// client := intern.NewGigsafeClient(conn)
 
-	client.GetEvents(context.Background(), &intern.Empty{})
+	// client.GetEvents(context.Background(), &intern.Empty{})
+	// server := intern.GigsafeServer{}
 }
